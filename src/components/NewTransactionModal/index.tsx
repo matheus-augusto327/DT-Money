082e10c1206187010996b4d6d1bd09c171489839
Matheus <matheus.augustoreal@gmail.com>
2022-09-20 08:40:34 -0300

    Creating accessible radio button and setting json-server

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,7 +2,6 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
 
-
 export function NewTransactionModal() {
   return (
     <Dialog.Portal>
@@ -21,12 +20,12 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Categoria" required />
 
           <TransactionType>
-            <TransactionTypeButton variant='income'>
+            <TransactionTypeButton variant='income' value='income'>
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
 
-            <TransactionTypeButton variant='outcome'>
+            <TransactionTypeButton variant='outcome' value='outcome'>
               <ArrowCircleDown size={24} />
               Saída
             </TransactionTypeButton>
